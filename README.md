bc82777668a9512139f5ac501d38497bf2351c9c
Adolfo <asanzdiego@gmail.com>
2015-11-23 11:26:37 +0100

    rename files

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # CasperJS Examples
 
+## Codemotion
+
+- Talk: <http://2015.codemotion.es/agenda.html#5699289732874240/45634007>
+- Slides:
+- Vídeo: -
+- Examples: <https://github.com/asanzdiego/casperjs-examples/tree/master/codemotion>
+
 ## t3chfest
 
 - Talk: <https://t3chfest.uc3m.es/2015/programa/casperjs-navega-con-el-piloto-automatico/>
 - Slides: <https://www.slideshare.net/asanzdiego/t3chfest-casperjs-navega-con-el-piloto-automtico>
 - Vídeo: <https://www.youtube.com/watch?v=YOuFYEbwGBc>
-- Examples:
-
-## Codemotion
-
-- Talk: <http://2015.codemotion.es/agenda.html#5699289732874240/45634007>
-- Slides:
-- Vídeo:
-- Examples:
+- Examples: <https://github.com/asanzdiego/casperjs-examples/tree/master/t3chfest>
---
+++ codemotion/06-from-files/06-from-file.js
@@ -0,0 +1,45 @@
+var casper = require('casper').create({
+    viewportSize: {
+        width: 1024,
+        height: 760
+    }
+});
+
+var fs = require('fs');
+var utils = require('utils');
+var data = fs.read('./06-files.json');
+var array = eval(data);
+
+casper.start('http://localhost:8080/', function() {
+
+    this.echo(this.getHTML('title'));
+    this.captureSelector('06-files-html.png', 'html');
+});
+
+array.forEach(function(text) {
+
+  casper.echo(text);
+
+  var author = "CasperJS";
+
+  casper.thenEvaluate(function(author, text) {
+      document.querySelector('input#author').setAttribute('value', author);
+      document.querySelector('input#text').setAttribute('value', text);
+  }, author, text);
+
+  casper.then(function() {
+
+      this.click('input#submit');
+  });
+
+  casper.waitFor(function check() {
+      return this.evaluate(function() {
+          return document.querySelectorAll('p#messages br').length > 1;
+      });
+  }, function then() {
+      this.captureSelector('06-files-p.png', 'p#messages');
+  });
+
+});
+
+casper.run();
